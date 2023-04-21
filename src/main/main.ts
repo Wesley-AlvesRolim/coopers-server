101ed21a9b7dbf4c083dedba57036e253eab52a2
Wesley Alves Rolim <walvesrolim@gmail.com>
2023-04-21 12:38:54 -0300

    chore: using PORT enviroment variable

--- src/main/main.ts
+++ src/main/main.ts
@@ -12,6 +12,6 @@ async function bootstrap() {
     allowedHeaders: 'Content-Type, Accept',
     credentials: true,
   });
-  await app.listen(3001);
+  await app.listen(process.env.PORT || 3001);
 }
 bootstrap();
